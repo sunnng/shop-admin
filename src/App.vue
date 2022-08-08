3258c0a15e380f4fe4bc6b041c4ae3c6d83b8721
sun <sunchengyuan0706@hotmail.com>
2022-08-08 15:22:32 +0800

    refactor: add scss 配置模块路径别名

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import HomeIndex from '@/views/home/HomeIndex.vue'
+
+console.log(HomeIndex)
+
 function printUser(
   firstName: string,
   lastName: string,
--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,12 @@ export default defineConfig({
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";'
+      }
+    }
   }
 })
