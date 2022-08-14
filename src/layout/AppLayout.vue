6947726efcf59236d8627b23ab626cd7ab89410a
诚远 <370281698@qq.com>
2022-08-14 11:24:18 +0800

    feat: 引入normalize.css；配置全局layout

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.el-container {
+  height: 100vh;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
+import 'normalize.css'
 import './styles/index.scss'
 
 const pinia = createPinia()
